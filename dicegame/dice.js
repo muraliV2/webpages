1e57b6908da0b10ef480005c10e2ccc695e47fa9
Murali <muralisurya2004@gmail.com>
2025-11-28 23:52:21 +0530

    Create dice.js

---
+++ dicegame/dice.js
@@ -0,0 +1,27 @@
+function rolldice()
+{
+let bx = document.getElementById("bx").value
+let values =[]
+let images =[]
+let btn = document.getElementById("btn")
+let im = document.getElementById("diceimages")
+let val = document.getElementById("diceresult")
+
+    for(let i =1;i<=bx;i++)
+    {
+        
+    const value = Math.floor(Math.random() * 6 + 1); 
+        images.push(`<img src="dice-images/${value}.png">`)
+        values.push(value);
+    }
+   val.textContent =`dice : ${values}`
+   im.innerHTML = images.join('')
+  
+
+
+}
+ 
+
+
+
+
