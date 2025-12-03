42edba3f6aa32961e055c5d1cab98f477b519c59
Murali <muralisurya2004@gmail.com>
2025-12-03 21:38:36 +0530

    Add palindrome checking functionality
    Implement palindrome check for input numbers.

---
+++ numberpalindrome/script.js
@@ -0,0 +1,27 @@
+
+let res = document.getElementById("res")
+
+
+function check()
+{
+let num = document.getElementById("inp").value
+num = Number(num)
+let rem=0;
+let rev=0;
+let copy = num
+while(num>0)
+{
+    rem = num % 10
+    rev = rev * 10 + rem
+    num = Math.floor(num /10)
+    console.log(rev)
+}
+if(copy == rev)
+{
+    res.textContent = "result : it is a palindrome number"
+}
+else
+{
+    res.textContent =  "result : it is not  a palindrome number"
+}
+}
