02ac4cc19a328480485de0e0c37a09f39fc51a81
Murali <muralisurya2004@gmail.com>
2025-12-04 22:04:58 +0530

    Create script.js

---
+++ stopwatch/script.js
@@ -0,0 +1,38 @@
+let Reqnum = 0;
+let interval = null;
+let time = document.getElementById("time")
+function padStart(value)
+{
+    return String(value).padStart(2,"0")
+}
+
+function Incre()
+{
+    const minutes = Math.floor(Reqnum /60);
+    const Seconds = Reqnum % 60;
+    time.innerHTML =    `${padStart(minutes)} : ${padStart(Seconds)}`
+
+}
+
+function ad()
+{
+    Reqnum++;
+    Incre();
+}
+function start()
+{
+    if(interval) stop()
+interval = setInterval(ad,1000)
+}
+function stop()
+{
+clearInterval(interval)
+}
+function reset()
+{
+    stop()
+     Reqnum = 0;
+    Incre()
+
+
+}
