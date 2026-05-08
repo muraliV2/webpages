67f2072cdc3a21a02bc37a8fd79991251fa88fab
Murali <muralisurya2004@gmail.com>
2026-05-08 20:30:11 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -7,8 +7,7 @@ let btn = document.getElementById("btn")
 let im = document.getElementById("diceimages")
 let val = document.getElementById("diceresult")
     for(let i =1;i<=bx;i++)
-    {
-        
+    {        
     const value = Math.floor(Math.random() * 6 + 1); 
         images.push(`<img src="dice-images/${value}.png">`)
         values.push(value);
